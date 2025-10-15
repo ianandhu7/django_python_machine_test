053d45d05b7fef035e8ec74f494e78be65fd56ad
anandhuviju <anandhuviju7@gmail.com>
2025-10-15 20:59:25 +0530

    Updated code after changes

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Category, Expense
 
-# Simple User registration serializer
+
 class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -10,18 +10,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
-        # use create_user to hash password
+        
         user_obj = User.objects.create_user(**validated_data)
         return user_obj
 
-# Simple user listing serializer
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ["id", "username", "email", "first_name", "last_name"]
 
-# Category serializer
+
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -29,7 +28,6 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["id"]
 
-# Expense serializer
 class ExpenseSerializer(serializers.ModelSerializer):
 
     class Meta:
