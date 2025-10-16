474a145f6c1aaf888a4d1e67ba8f2eeac0f4f1f7
anandhuviju <anandhuviju7@gmail.com>
2025-10-16 23:08:44 +0530

    Update code with latest changes

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -7,7 +7,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "first_name", "last_name"]
+        fields = ["username", "email",  "first_name", "last_name"]
 
     def create(self, validated_data):
         
--- myapp/views.py
+++ myapp/views.py
@@ -2,24 +2,24 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.shortcuts import get_object_or_404
 from .serializers import RegisterSerializer, UserSerializer
 from .models import Category, Expense
-from django.db.models import Sum
 
 
 class RegisterView(APIView):
-
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
+            data = serializer.save()
             return Response(
                 {
                     "message": "User registered successfully",
                     "data": {
-                        "id": user.id,
-                        "username": user.username,
-                        "email": user.email
+                        "id": data.id,
+                        "username": data.username,
+                        "email": data.email
                     }
                 },
                 status=status.HTTP_201_CREATED
@@ -29,50 +29,55 @@ class RegisterView(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-class UserListCreateView(APIView):
 
+class UserListCreateView(APIView):
     def get(self, request):
         data = User.objects.all()
         serializer = UserSerializer(data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"message": "Creation failed"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserRetrieveUpdateDeleteView(APIView):
 
     def get(self, request, **kwargs):
         id = kwargs.get('pk')
-        user = User.objects.get(id=id)
-        serializer = UserSerializer(user)
+        data = get_object_or_404(User, id=id)
+        serializer = UserSerializer(data)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
     def put(self, request, **kwargs):
         id = kwargs.get('pk')
-        user = User.objects.get(id=id)
-        serializer = UserSerializer(user, data=request.data)
+        existing = get_object_or_404(User, id=id)
+        serializer = UserSerializer(existing, data=request.data,partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response("Update failed", status=status.HTTP_400_BAD_REQUEST)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"message": "Update failed", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
     def delete(self, request, **kwargs):
         id = kwargs.get('pk')
-        user = User.objects.get(id=id)
-        user.delete()
-        return Response("Deleted successfully", status=status.HTTP_200_OK)
+        data = get_object_or_404(User, id=id)
+        data.delete()
+        return Response({"message": "Deleted successfully"}, status=status.HTTP_200_OK)
 
 
 class ExpenseSummaryView(APIView):
-    
-    
     def get(self, request, **kwargs):
         category_id = kwargs.get('pk')
-        category = Category.objects.filter(id=category_id).first()
-        
-        if not category:
-            
-            return Response({"error": "Category not found"})
-
-        
-        total = Expense.objects.filter(category=category).aggregate(total_amount=Sum('amount'))['total_amount'] or 0
-
-        return Response({category.name: int(total)})
+        category = get_object_or_404(Category, id=category_id)
+        total = Expense.objects.filter(category=category).aggregate(
+            total_amount=Sum('amount')
+        )['total_amount'] or 0
+        return Response({category.name: float(total)})
